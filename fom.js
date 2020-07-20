265a912750e52e72e11e93d26b277c7223ce66af
ZEN_98 <61709488+PiyushPatil9898@users.noreply.github.com>
2020-07-20 11:25:35 +0530

    form

---
+++ fom.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+// >> Target
+const nam_F=document.querySelector("#F_txt");
+const nam_L=document.querySelector("#L_txt");
+const nam_E=document.querySelector("#e_txt");
+const sel=document.querySelector("#sel");
+const mes=document.querySelector("#mes");
+const but=document.querySelector("#but");
+// >> Input
+
+//>> submit
+but.onclick=()=>{
+};
